4f481d751ff8697ccfa0868fc698f53ccb115e3a
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-03 20:17:38 +0300

    Refactors app structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route} from 'react-router-dom'
-import RestaurantPage from "./RestaurantsPage";
-import Landing from "./Landing";
-import Pizzas from './Pizzas';
-import PostRestaurantPizzas from './PostRestaurantPizzas';
-import Contact from './Contact';
+import Home from './components/Home';
+import RestaurantPage from "./components/RestaurantsPage";
+import Landing from "./components/Landing";
+import Pizzas from './components/Pizzas';
+import PostRestaurantPizzas from './components/PostRestaurantPizzas';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="bg-slate-500 min-h-screen">
     <Routes>
       <Route element= {<Landing/>}>
-        <Route path= "/" element={<RestaurantPage />} />
+        <Route path= "/" element={<Home />} />
+        <Route path= "/restaurants" element={<RestaurantPage />} />
         <Route path= "/pizzas" element={<Pizzas />} />
         <Route path= "/restaurantPizzas" element={<PostRestaurantPizzas />} />
         <Route path= "/contact" element={<Contact />} />
