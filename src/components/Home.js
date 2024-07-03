b2d357bce145a520a010d68a62f3bd5a4355f9f6
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-03 20:19:06 +0300

    creates Home component

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <h1 className="text-5xl font-medium text-indigo-700 text-center">This is Home</h1>
+        </div>
+     );
+}
+ 
+export default Home;
