efbf856092d12a813d86085a83a53004762ec6cc
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-03 20:27:14 +0300

    adds padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="container">
+        <div className="container p-10">
             <h1 className="text-5xl font-medium text-indigo-700 text-center">This is Home</h1>
         </div>
      );
