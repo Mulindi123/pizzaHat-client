fa0b6bc3c91062e45d4834480b4895a13a50471a
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-06 12:59:26 +0300

    styles the navbar

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,42 +1,41 @@
-import { Outlet} from "react-router-dom";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    // BreadcrumbSeparator,
-    Button
-  } from '@chakra-ui/react'
-import { ChevronRightIcon} from "@chakra-ui/icons"
+import { Outlet } from "react-router-dom";
+// import { ChevronRightIcon } from "@chakra-ui/icons";
 
-
-
-function Landing(){
-    return (
-        <div className="">
-            <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' />}>
-                <BreadcrumbItem p={4}>
-                    <Button colorScheme='purple' p={4}><BreadcrumbLink href='/'>Home</BreadcrumbLink></Button>
-                </BreadcrumbItem>
-                <BreadcrumbItem p={4}>
-                    <Button colorScheme='purple' p={4}><BreadcrumbLink href='/restaurants'>Restaurants</BreadcrumbLink></Button>
-                </BreadcrumbItem>
-
-                <BreadcrumbItem p={4}>
-                    <Button colorScheme="purple" p={4}><BreadcrumbLink href='/pizzas'>Pizza</BreadcrumbLink></Button>
-                </BreadcrumbItem>
-
-                <BreadcrumbItem p={4}>
-                    <Button colorScheme="purple" p={4}><BreadcrumbLink href='/restaurantPizzas'>Add Pizza</BreadcrumbLink></Button>
-                </BreadcrumbItem>
-
-                <BreadcrumbItem p={4}>
-                    <Button colorScheme="purple" p={4} value={'outline'} className="m-2"><BreadcrumbLink href='/contact'>Contact</BreadcrumbLink></Button>
-                </BreadcrumbItem>
-            </Breadcrumb>
-            <div className="">
-                <Outlet />
-            </div>
+function Landing() {
+  return (
+    <div className="min-h-screen p-4 bg-gradient-to-b">
+      <nav className="flex flex-wrap justify-center mb-4">
+        <div className="p-2 md:p-4">
+          <a href="/" className="inline-block px-4 py-2 text-white border rounded-lg border-black hover:bg-purple-600">
+            Home
+          </a>
         </div>
-    )
+        <div className="p-2 md:p-4">
+          <a href="/restaurants" className="inline-block px-4 py-2 text-white border rounded-lg border-black hover:bg-purple-600">
+            Restaurants
+          </a>
+        </div>
+        <div className="p-2 md:p-4">
+          <a href="/pizzas" className="inline-block px-4 py-2 text-white border rounded-lg border-black hover:bg-purple-600">
+            Pizza
+          </a>
+        </div>
+        <div className="p-2 md:p-4">
+          <a href="/restaurantPizzas" className="inline-block px-4 py-2 text-white border rounded-lg border-black hover:bg-purple-600">
+            Add Pizza
+          </a>
+        </div>
+        <div className="p-2 md:p-4">
+          <a href="/contact" className="inline-block px-4 py-2 text-white rounded-lg hover:bg-purple-600 border border-black">
+            Contact
+          </a>
+        </div>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
-export default Landing
+
+export default Landing;
